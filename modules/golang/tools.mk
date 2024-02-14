1aef6c37fa1edeb8cce3dabcb2c794c6956274f3
eric-reis <53226982+eric-reis@users.noreply.github.com>
2024-02-14 09:14:04 -0300

    The go tools have been updated to newer versions: (#36)
    * golangci-lint: v1.55.2 to v1.56.1
    
    * goose: v3.16.0 to v3.18.0

--- modules/golang/tools.mk
+++ modules/golang/tools.mk
@@ -15,11 +15,11 @@ GO_TOOLS_SWAGGER_VERSION ?= v0.30.5
 
 ## Selects golangci-lint version.
 ## https://github.com/golangci/golangci-lint/releases
-GO_TOOLS_GOLANGCI_VERSION ?= 1.55.2
+GO_TOOLS_GOLANGCI_VERSION ?= 1.56.1
 
 ## Selects goose version.
 ## https://github.com/pressly/goose/releases
-GO_TOOLS_GOOSE_VERSION ?= v3.16.0
+GO_TOOLS_GOOSE_VERSION ?= v3.18.0
 
 ## Selects go-junit-report version.
 ## https://github.com/jstemmer/go-junit-report/releases
